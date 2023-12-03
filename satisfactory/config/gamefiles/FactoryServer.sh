bac47c0b5f4cb2c5d2e6bcfe8ca1285d6f0c1b64
pisel <schultz.thomas@free.fr>
2023-12-03 15:12:32 +0100

    add install assetto

--- satisfactory/config/gamefiles/FactoryServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
-chmod +x "$UE_PROJECT_ROOT/Engine/Binaries/Linux/UnrealServer-Linux-Shipping"
-"$UE_PROJECT_ROOT/Engine/Binaries/Linux/UnrealServer-Linux-Shipping" FactoryGame "$@" 
