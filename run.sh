18b784ea3bbf9216c077181ad35639a26852a488
pisel <schultz.thomas@free.fr>
2025-01-25 20:50:50 +0100

    restore minecraft and prepare for bot docker

--- run.sh
+++ run.sh
@@ -7,15 +7,26 @@ if [ ! -d "$COMPOSE_DIR" ]; then
   exit 1
 fi
 
-for COMPOSE_FILE in "$COMPOSE_DIR"/*/docker-compose.yml; do
-  if [ -f "$COMPOSE_FILE" ]; then
-    echo "Lancement de Docker Compose dans : $(dirname "$COMPOSE_FILE")"
-    pushd "$(dirname "$COMPOSE_FILE")" > /dev/null
-    docker compose up -d
-    popd > /dev/null
-  else
-    echo "Aucun fichier docker-compose.yml trouvé dans $COMPOSE_DIR."
-  fi
-done
+function run_docker_compose {
+  local dir=$1
+  for COMPOSE_FILE in "$dir"/*/docker-compose.yml; do
+    if [ -f "$COMPOSE_FILE" ]; then
+      echo "Lancement de Docker Compose dans : $(dirname "$COMPOSE_FILE")"
+      pushd "$(dirname "$COMPOSE_FILE")" > /dev/null
+      docker compose up -d
+      popd > /dev/null
+    else
+      echo "Aucun fichier docker-compose.yml trouvé dans $dir."
+    fi
+  done
+
+  for subdir in "$dir"/*/; do
+    if [ -d "$subdir" ]; then
+      run_docker_compose "$subdir"
+    fi
+  done
+}
+
+run_docker_compose "$COMPOSE_DIR"
 
 echo "Tous les conteneurs Docker ont été lancés."
